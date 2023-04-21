6f7741b5b0a888b370de995caf11d7132cece721
SaneMyburg <sane.myburg@gmail.com>
2023-04-21 02:28:36 +0200

    Add styles to details page

--- src/components/ForexDetails.js
+++ src/components/ForexDetails.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FaMicrophone } from 'react-icons/fa';
 import { IoIosArrowBack } from 'react-icons/io';
 import { FiSettings } from 'react-icons/fi';
+import '../styles/details.css';
 
 const ForexDetails = () => {
   const { selectedPair } = useSelector((state) => state.currencies);
@@ -12,41 +13,50 @@ const ForexDetails = () => {
   }
 
   return (
-    <div>
-      <div>
+    <div className="details-container">
+      <div className="details-top">
         <Link to="/">
           <IoIosArrowBack />
         </Link>
         <p>{selectedPair[0].ticker}</p>
-        <FaMicrophone />
-        <FiSettings />
+        <div className="extra-icons">
+          <FaMicrophone />
+          <FiSettings />
+        </div>
       </div>
-      <h1>
-        {selectedPair[0].ticker}
-      </h1>
-      <div>
-        Open:
-        {selectedPair[0].open}
+      <div className="details-p">
+        <h1>
+          {selectedPair[0].ticker}
+        </h1>
       </div>
-      <div>
-        Bid:
-        {selectedPair[0].bid}
+      <div className="details-para">
+        <p>FOREX PAIR DETAILS</p>
       </div>
-      <div>
-        Ask:
-        {selectedPair[0].ask}
-      </div>
-      <div>
-        High:
-        {selectedPair[0].high}
-      </div>
-      <div>
-        Low:
-        {selectedPair[0].low}
-      </div>
-      <div>
-        Changes:
-        {selectedPair[0].changes.toFixed(3)}
+      <div className="details-div">
+        <div className="details-div-child">
+          <p className="first-p">Open</p>
+          <p className="second-p">{selectedPair[0].open}</p>
+        </div>
+        <div className="details-div-child">
+          <p className="first-p">Bid</p>
+          <p className="second-p">{selectedPair[0].bid}</p>
+        </div>
+        <div className="details-div-child">
+          <p className="first-p">Ask</p>
+          <p className="second-p">{selectedPair[0].ask}</p>
+        </div>
+        <div className="details-div-child">
+          <p className="first-p">High</p>
+          <p className="second-p">{selectedPair[0].high}</p>
+        </div>
+        <div className="details-div-child">
+          <p className="first-p">Low</p>
+          <p className="second-p">{selectedPair[0].low}</p>
+        </div>
+        <div className="details-div-child">
+          <p className="first-p">Changes</p>
+          <p className="second-p">{selectedPair[0].changes.toFixed(3)}</p>
+        </div>
       </div>
     </div>
   );
